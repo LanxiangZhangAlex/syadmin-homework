3ac609bb96a834fdf026502d71913cb78f57a798
Lanxiang Zhang <zlx930422@gmail.com>
2026-07-20 23:33:05 +0300

    fix: pass DynamoDB policy into Lambda module
    Define the table-scoped read policy in the root module where the DynamoDB resource is available.

--- terraform/webserver.tf
+++ terraform/webserver.tf
@@ -7,9 +7,19 @@ data "local_file" "source_webserver" {
 }
 
 module "webserver_lambda" {
-  source         = "./modules/terraform-aws-lambda-py"
-  function_name  = local.webserver_function_name
-  description    = "Web server"
-  source_code    = data.local_file.source_webserver.content
-  tags           = var.tags
+  source        = "./modules/terraform-aws-lambda-py"
+  function_name = local.webserver_function_name
+  description   = "Web server"
+  source_code   = data.local_file.source_webserver.content
+  tags          = var.tags
+  custom_policies = {
+    DynamoDBRead = jsonencode({
+      Version = "2012-10-17"
+      Statement = [{
+        Action   = ["dynamodb:GetItem", "dynamodb:Scan"]
+        Effect   = "Allow"
+        Resource = aws_dynamodb_table.text_table.arn
+      }]
+    })
+  }
 }
